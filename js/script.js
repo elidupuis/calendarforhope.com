4e4da88b6f63eccc4f64fcc8cbcc18db2231b5d4
Eli Dupuis <elidupuis@gmail.com>
2011-10-28 23:12:13 -0600

    style and functionality tweaks

--- js/script.js
+++ js/script.js
@@ -15,7 +15,8 @@ $(document).ready(function(){
 	//  thanks modal
 	if (window.location.hash === '#thanks') {
 	  window.location.hash = '';
-	  var modal = $('<div id="thanks-modal">').html('<p><em>Thanks for donating and supporting such a great cause. You\'re awesome!</em></p><p>We\'ll get your order in mail as soon as possible.</p><p><a class="close" href="#">Close this message &raquo;</a></p>');
+	  // var modal = $('<div id="thanks-modal">').html('<p><em>Thanks for donating and supporting such a great cause. You\'re awesome!</em></p><p>We\'ll get your order in mail as soon as possible.</p><p><a class="close" href="#">Close this message &raquo;</a></p>');
+	  var modal = $('<div id="thanks-modal">').html('<p><em>Thanks for ordering calendar(s)!</em></p><p>They\'ll be hot off the press November 8th, and we\'ll be shipping them Novemeber 9th.</p><p><a class="close" href="#">Close this message &raquo;</a></p>');
 	  $('body').append(modal);
 	  modal.find('a').bind('click', function(){
 	    modal.remove();
