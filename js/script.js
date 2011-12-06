9bc1d968ea377e16f961da7912cc4542333c53a8
Eli Dupuis <elidupuis@gmail.com>
2011-12-05 18:16:43 -0700

    update thanks modal message. remove unneeded plugins

--- js/script.js
+++ js/script.js
@@ -1,22 +1,10 @@
 $(document).ready(function(){
-	jQuery.easing.def = "easeOutExpo";
-	
-	$('nav a').click(function(){
-		var target = $($(this).attr('href'));
-		$(document).scrollTo(target, 750, {offset:-20});		
-		return false;
-	});
-
-  // fixed nav
-  $('nav ul').stickybox({
-    offset: 10
-  });
 
 	//  thanks modal
 	if (window.location.hash === '#thanks') {
 	  window.location.hash = '';
-	  // var modal = $('<div id="thanks-modal">').html('<p><em>Thanks for donating and supporting such a great cause. You\'re awesome!</em></p><p>We\'ll get your order in mail as soon as possible.</p><p><a class="close" href="#">Close this message &raquo;</a></p>');
-	  var modal = $('<div id="thanks-modal">').html('<p><em>Thanks for ordering calendar(s)!</em></p><p>They\'ll be hot off the press November 8th, and we\'ll be shipping them Novemeber 9th.</p><p><a class="close" href="#">Close this message &raquo;</a></p>');
+    var modal = $('<div id="thanks-modal">').html('<p><em>Thanks for donating and supporting such a great cause. You\'re awesome!</em></p><p>Your order will be shipped within 2 business days. Please allow 3-5 days for regular postal delivery.</p><p><a class="close" href="#">Close this message &raquo;</a></p>');
+    // var modal = $('<div id="thanks-modal">').html('<p><em>Thanks for ordering calendar(s)!</em></p><p>They\'ll be hot off the press November 8th, and we\'ll be shipping them Novemeber 9th.</p><p><a class="close" href="#">Close this message &raquo;</a></p>');      
 	  $('body').append(modal);
 	  modal.find('a').bind('click', function(){
 	    modal.remove();
