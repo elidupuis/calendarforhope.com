a26fe0d9b1b75d27e09fc254373a22f813ad52fb
Eli Dupuis <elidupuis@gmail.com>
2013-10-21 23:19:26 -0600

    add readme

---
+++ README.md
@@ -0,0 +1,12 @@
+# Calendar for Hope Website
+
+## Local Dev
+
+    bundle install
+    jekyll serve -w
+
+
+## Compile Less to CSS
+
+    npm install -g less
+    lessc css/style.less > css/style.css
--- js/script.js
+++ js/script.js
@@ -3,8 +3,8 @@ $(document).ready(function(){
 	//  thanks modal
 	if (window.location.hash === '#thanks') {
 	  window.location.hash = '';
-    var modal = $('<div id="thanks-modal">').html('<p><em>Thanks for donating and supporting such a great cause. You\'re awesome!</em></p><p>Your order will be shipped within 2 business days. Please allow 3-5 days for regular postal delivery.</p><p><a class="close" href="#">Close this message &raquo;</a></p>');
-    // var modal = $('<div id="thanks-modal">').html('<p><em>Thanks for ordering calendar(s)!</em></p><p>They\'ll be hot off the press November 8th, and we\'ll be shipping them Novemeber 9th.</p><p><a class="close" href="#">Close this message &raquo;</a></p>');      
+    // var modal = $('<div id="thanks-modal">').html('<p><em>Thanks for donating and supporting such a great cause. You\'re awesome!</em></p><p>Your order will be shipped within 2 business days. Please allow 3-5 days for regular postal delivery (in Canada).</p><p><a class="close" href="#">Close this message &raquo;</a></p>');
+    var modal = $('<div id="thanks-modal">').html('<p><em>Thanks for ordering calendar(s)!</em></p><p>They\'ll be hot off the press in late October, and we\'ll be shipping them in early Novemeber.</p><p><a class="close" href="#">Close this message &raquo;</a></p>');
 	  $('body').append(modal);
 	  modal.find('a').bind('click', function(){
 	    modal.remove();
@@ -12,5 +12,5 @@ $(document).ready(function(){
 	  });
 	};
 
-	
+
 });
